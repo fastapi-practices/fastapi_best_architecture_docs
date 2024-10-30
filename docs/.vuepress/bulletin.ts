2462d73c364c933f9ca8f5ff85c0b16b5d50f636
Wu Clan <jianhengwu0407@gmail.com>
2024-10-30 19:25:52 +0800

    update docs

--- docs/.vuepress/bulletin.ts
+++ docs/.vuepress/bulletin.ts
@@ -4,15 +4,18 @@ export const myBulletin: BulletinOptions = {
     enablePage: true,
     title: '欢迎加入我们',
     content: `
-::: center
+免费订阅：
 
 **Discord 服务器：** [Python 开发交流社区](https://discord.com/invite/yNN3wTbVAC)
 
-<hr>
+<hr/>
 
-<iframe src="https://discord.com/widget?id=1185035164577972344&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+付费订阅：
 
-:::
+**知识星球：** [FBA](https://t.zsxq.com/sDbws)
+
+<img src="https://wu-clan.github.io/picx-images-hosting/星球优惠券.png">
+
 `,
     contentType: 'markdown'
 }
