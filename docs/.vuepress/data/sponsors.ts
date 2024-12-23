3ed20a6418e82b6aea824f8d3e14615e8a85f699
Wu Clan <jianhengwu0407@gmail.com>
2024-12-24 01:33:46 +0800

    update docs

--- docs/.vuepress/data/sponsors.ts
+++ docs/.vuepress/data/sponsors.ts
@@ -4,19 +4,19 @@ export const goldSponsors = ref([
     {
         link: '',
         text: '成为赞助商',
-        href: 'https://discord.com/invite/yNN3wTbVAC',
+        href: 'https://fastapi-practices.github.io/fastapi_best_architecture_docs/sponsors.html#%E5%B1%95%E4%BD%8D%E8%B5%9E%E5%8A%A9%E5%95%86',
         alt: 'wu-clan',
     },
     {
         link: '',
         text: '成为赞助商',
-        href: 'https://discord.com/invite/yNN3wTbVAC',
+        href: 'https://fastapi-practices.github.io/fastapi_best_architecture_docs/sponsors.html#%E5%B1%95%E4%BD%8D%E8%B5%9E%E5%8A%A9%E5%95%86',
         alt: 'wu-clan',
     },
     {
         link: '',
         text: '成为赞助商',
-        href: 'https://discord.com/invite/yNN3wTbVAC',
+        href: 'https://fastapi-practices.github.io/fastapi_best_architecture_docs/sponsors.html#%E5%B1%95%E4%BD%8D%E8%B5%9E%E5%8A%A9%E5%95%86',
         alt: 'wu-clan',
     }
 ])
@@ -25,37 +25,37 @@ export const generalSponsors = ref([
     {
         link: '',
         text: '成为赞助商',
-        href: 'https://discord.com/invite/yNN3wTbVAC',
+        href: 'https://fastapi-practices.github.io/fastapi_best_architecture_docs/sponsors.html#%E5%B1%95%E4%BD%8D%E8%B5%9E%E5%8A%A9%E5%95%86',
         alt: 'wu-clan',
     },
     {
         link: '',
         text: '成为赞助商',
-        href: 'https://discord.com/invite/yNN3wTbVAC',
+        href: 'https://fastapi-practices.github.io/fastapi_best_architecture_docs/sponsors.html#%E5%B1%95%E4%BD%8D%E8%B5%9E%E5%8A%A9%E5%95%86',
         alt: 'wu-clan',
     },
     {
         link: '',
         text: '成为赞助商',
-        href: 'https://discord.com/invite/yNN3wTbVAC',
+        href: 'https://fastapi-practices.github.io/fastapi_best_architecture_docs/sponsors.html#%E5%B1%95%E4%BD%8D%E8%B5%9E%E5%8A%A9%E5%95%86',
         alt: 'wu-clan',
     },
     {
         link: '',
         text: '成为赞助商',
-        href: 'https://discord.com/invite/yNN3wTbVAC',
+        href: 'https://fastapi-practices.github.io/fastapi_best_architecture_docs/sponsors.html#%E5%B1%95%E4%BD%8D%E8%B5%9E%E5%8A%A9%E5%95%86',
         alt: 'wu-clan',
     },
     {
         link: '',
         text: '成为赞助商',
-        href: 'https://discord.com/invite/yNN3wTbVAC',
+        href: 'https://fastapi-practices.github.io/fastapi_best_architecture_docs/sponsors.html#%E5%B1%95%E4%BD%8D%E8%B5%9E%E5%8A%A9%E5%95%86',
         alt: 'wu-clan',
     },
     {
         link: '',
         text: '成为赞助商',
-        href: 'https://discord.com/invite/yNN3wTbVAC',
+        href: 'https://fastapi-practices.github.io/fastapi_best_architecture_docs/sponsors.html#%E5%B1%95%E4%BD%8D%E8%B5%9E%E5%8A%A9%E5%95%86',
         alt: 'wu-clan',
     }
 ])
