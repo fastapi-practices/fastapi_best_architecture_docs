6667b04e6bbaf5d5003b74df680b6a5b3c56bbd2
Wu Clan <jianhengwu0407@gmail.com>
2025-05-16 14:45:59 +0800

    update docs

--- docs/.vuepress/bulletin.ts
+++ docs/.vuepress/bulletin.ts
@@ -1,9 +1,16 @@
 import { BulletinOptions } from "vuepress-theme-plume";
 
 export const myBulletin: BulletinOptions = {
-    enablePage: false,
+    enablePage: true,
     lifetime: 'session',
     contentType: 'markdown',
     title: '公告',
-    content: '插件系统已发布, 欢迎体验和共创 🤗',
+    content: `\
+**v1.3.0** 版本已发布，涉及重大变更 💥
+
+- app 配置由独立控制改为统一控制
+- 插件系统焕新升级，涉及插件配置、安装方式、新增接口
+
+以上变更内容，可通过文档/源码自行查看
+`,
 }
