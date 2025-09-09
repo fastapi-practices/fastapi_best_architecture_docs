b44251370bb95766f0060e090f43a81c28d9c87d
Wu Clan <jianhengwu0407@gmail.com>
2025-09-09 19:13:47 +0800

    update docs

--- docs/.vuepress/bulletin.ts
+++ docs/.vuepress/bulletin.ts
@@ -9,7 +9,7 @@ export const myBulletin: BulletinOptions = {
     title: '公告',
     content: `\
 ::: center
-🎉 v1.9.0 已发布 🎉
+🎉 v1.8.1 已发布 🎉
 :::
 
 针对此版本，我们为大家带来了：
@@ -17,7 +17,5 @@ export const myBulletin: BulletinOptions = {
 - Google OAuth2
 - 代码生成 CLI
 - 优化架构、bug 修复
-
-为大家带来以上惊喜的同时，前端实施也在悄悄推进中，==代码生成页面已完成=={.important}，快快[本地部署](https://fastapi-practices.github.io/fastapi_best_architecture_docs/frontend/summary/quick-start.html)尝鲜吧~
 `,
 }
