9ab51e66a29d1696604817fc6ba8cbb734af8e74
Wu Clan <jianhengwu0407@gmail.com>
2026-04-02 16:27:03 +0800

    update docs

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -1,254 +1,259 @@
 import { defineNavbarConfig, ThemeNavItem } from "vuepress-theme-plume";
 
 export const myNavbar: ThemeNavItem[] = defineNavbarConfig([
-    {
-        text: 'Guide',
+  {
+    text: "Guide",
+    items: [
+      {
+        text: "工程化",
         items: [
-            {
-                text: '工程化',
-                items: [
-                    {
-                        text: 'CLI',
-                        icon: 'glyphs:cli-bold',
-                        link: '/cli'
-                    }
-                ]
-            },
-            {
-                text: '后端',
-                items: [
-                    {
-                        text: '快速开始',
-                        icon: 'material-symbols:start',
-                        link: '/backend/summary/quick-start'
-                    },
-                    {
-                        text: '接口文档',
-                        icon: 'simple-icons:apifox',
-                        link: 'https://apifox.com/apidoc/shared-28a93f02-730b-4f33-bb5e-4dad92058cc0'
-                    },
-                ]
-            },
-            {
-                text: '前端',
-                items: [
-                    {
-                        text: 'Arco UI',
-                        badge: { text: '已弃用', type: 'danger' },
-                        icon: 'icon-park-outline:new-lark',
-                        link: '/frontend/summary/arco',
-                    },
-                    {
-                        text: 'Vben UI',
-                        icon: 'simple-icons:antdesign',
-                        link: '/frontend/summary/intro'
-                    },
-                ]
-            },
-            {
-                text: '更多',
-                items: [
-                    {
-                        text: '更新记录',
-                        icon: 'stash:last-updates-duotone',
-                        link: 'https://github.com/fastapi-practices/fastapi-best-architecture/blob/master/CHANGELOG.md'
-                    },
-                    {
-                        text: '参与贡献',
-                        icon: 'mdi:people-add',
-                        link: 'https://github.com/fastapi-practices/fastapi-best-architecture/tree/master/backend#contributing'
-                    },
-                ]
-            },
-        ]
-    },
-    {
-        text: 'AI 赋能',
-        items: [
-            {
-                text: 'Skills',
-                icon: 'hugeicons:language-skill',
-                link: '/ai/skills',
-            },
-            {
-                text: 'MCP',
-                icon: 'gravity-ui:logo-mcp',
-                link: '/ai/mcp',
-            },
-            {
-                text: 'Prompt',
-                icon: 'streamline:ai-prompt-spark-remix',
-                link: '/ai/prompt',
-            },
-            {
-                text: 'llms.txt',
-                icon: 'octicon:ai-model-16',
-                link: '/ai/llms',
-            },
-        ]
-    },
-    {
-        text: '生态系统',
-        items: [
-            {
-                text: '资源',
-                items: [
-                    // {
-                    //     text: '架构',
-                    //     icon: 'carbon:ibm-deployable-architecture',
-                    //     link: '/architecture'
-                    // },
-                    {
-                        text: '技术栈',
-                        icon: 'material-symbols:stack',
-                        link: '/stack'
-                    },
-                    {
-                        text: '官方开源',
-                        icon: 'mdi:github',
-                        link: '/open-source'
-                    },
-                    {
-                        text: '社区开源',
-                        icon: 'mdi:github',
-                        link: '/community-open-source'
-                    },
-                ]
-            },
-            {
-                text: '互动',
-                items: [
-                    {
-                        text: '交流群',
-                        icon: 'ic:baseline-wechat',
-                        link: '/group'
-                    },
-                    {
-                        text: '作者主页',
-                        icon: 'streamline-plump:browser-website-1-remix',
-                        link: 'https://wu-clan.github.io/homepage'
-                    },
-                    {
-                        text: 'Github 问题',
-                        icon: 'codicon:issues',
-                        link: 'https://github.com/fastapi-practices/fastapi-best-architecture/issues'
-                    },
-                    {
-                        text: 'Github 讨论',
-                        icon: 'mdi:discussion',
-                        link: 'https://github.com/fastapi-practices/fastapi-best-architecture/discussions'
-                    },
-                    {
-                        text: '想法和建议',
-                        icon: 'mdi:discussion',
-                        link: 'https://discord.gg/xp8M6nY4NA'
-                    },
-                    {
-                        text: '我要催更',
-                        icon: 'material-symbols:update-rounded',
-                        link: 'https://discord.gg/JyedBeHXkn'
-                    },
-                ]
-            },
-            {
-                text: '课程',
-                items: [
-                    {
-                        text: '视频课程',
-                        badge: { text: '已过时', type: 'danger' },
-                        icon: 'ri:bilibili-fill',
-                        link: 'https://space.bilibili.com/284237214/lists'
-                    },
-                ]
-            },
-            {
-                text: '动态',
-                items: [
-                    {
-                        text: '博客',
-                        badge: { text: '充电站', type: 'tip' },
-                        icon: 'fa7-solid:blog',
-                        link: '/blog/'
-                    }
-                ]
-            }
-        ]
-    },
-    {
-        text: '插件系统',
-        icon: 'unjs:unplugin',
-        items: [
-            {
-                text: '文档',
-                items: [
-                    {
-                        text: '前言',
-                        icon: 'bi:chat-text',
-                        link: '/plugin/before'
-                    },
-                    {
-                        text: '插件开发',
-                        icon: 'la:dev',
-                        link: '/plugin/dev'
-                    },
-                    {
-                        text: '插件分享',
-                        icon: 'material-symbols:share',
-                        link: '/plugin/share'
-                    },
-                    {
-                        text: '插件安装',
-                        icon: 'icon-park-outline:install',
-                        link: '/plugin/install'
-                    }
-                ]
-            },
-            {
-                text: '更多',
-                items: [
-                    {
-                        text: '插件市场',
-                        icon: 'mdi:marketplace-outline',
-                        // badge: { text: 'HOT', type: 'danger' },
-                        link: '/marketplace'
-                    },
-                ]
-            }
-        ]
-    },
-    {
-        text: 'Pricing',
-        icon: 'streamline-color:bag-dollar',
-        link: '/pricing'
-    },
-    {
-        text: '赞助',
-        icon: 'material-icon-theme:github-sponsors',
-        link: '/sponsors'
-    },
-    {
-        text: '关于',
-        items: [
-            {
-                text: '常见问题',
-                icon: 'healthicons:question',
-                link: '/questions.md'
-            },
-            {
-                text: '用户登记',
-                icon: 'mdi:user-check',
-                link: '/users'
-            },
-            {
-                text: '社区团队',
-                icon: 'fa-solid:user-friends',
-                link: '/team'
-            },
-            {
-                text: '隐私政策',
-                icon: 'iconoir:privacy-policy',
-                link: '/privacy-policy'
-            },
+          {
+            text: "架构",
+            icon: "hugeicons:structure-01",
+            link: "/architecture",
+          },
+          {
+            text: "CLI",
+            icon: "glyphs:cli-bold",
+            link: "/cli",
+          },
         ],
-    }
-])
+      },
+      {
+        text: "后端",
+        items: [
+          {
+            text: "快速开始",
+            icon: "material-symbols:start",
+            link: "/backend/summary/quick-start",
+          },
+          {
+            text: "接口文档",
+            icon: "simple-icons:apifox",
+            link: "https://apifox.com/apidoc/shared-28a93f02-730b-4f33-bb5e-4dad92058cc0",
+          },
+        ],
+      },
+      {
+        text: "前端",
+        items: [
+          {
+            text: "Arco UI",
+            badge: { text: "已弃用", type: "danger" },
+            icon: "icon-park-outline:new-lark",
+            link: "/frontend/summary/arco",
+          },
+          {
+            text: "Vben UI",
+            icon: "simple-icons:antdesign",
+            link: "/frontend/summary/intro",
+          },
+        ],
+      },
+      {
+        text: "更多",
+        items: [
+          {
+            text: "更新记录",
+            icon: "stash:last-updates-duotone",
+            link: "https://github.com/fastapi-practices/fastapi-best-architecture/blob/master/CHANGELOG.md",
+          },
+          {
+            text: "参与贡献",
+            icon: "mdi:people-add",
+            link: "https://github.com/fastapi-practices/fastapi-best-architecture/tree/master/backend#contributing",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    text: "AI 赋能",
+    items: [
+      {
+        text: "Skills",
+        icon: "hugeicons:language-skill",
+        link: "/ai/skills",
+      },
+      {
+        text: "MCP",
+        icon: "gravity-ui:logo-mcp",
+        link: "/ai/mcp",
+      },
+      {
+        text: "Prompt",
+        icon: "streamline:ai-prompt-spark-remix",
+        link: "/ai/prompt",
+      },
+      {
+        text: "llms.txt",
+        icon: "octicon:ai-model-16",
+        link: "/ai/llms",
+      },
+    ],
+  },
+  {
+    text: "生态系统",
+    items: [
+      {
+        text: "资源",
+        items: [
+          // {
+          //     text: '架构',
+          //     icon: 'carbon:ibm-deployable-architecture',
+          //     link: '/architecture'
+          // },
+          {
+            text: "技术栈",
+            icon: "material-symbols:stack",
+            link: "/stack",
+          },
+          {
+            text: "官方开源",
+            icon: "mdi:github",
+            link: "/open-source",
+          },
+          {
+            text: "社区开源",
+            icon: "mdi:github",
+            link: "/community-open-source",
+          },
+        ],
+      },
+      {
+        text: "互动",
+        items: [
+          {
+            text: "交流群",
+            icon: "ic:baseline-wechat",
+            link: "/group",
+          },
+          {
+            text: "作者主页",
+            icon: "streamline-plump:browser-website-1-remix",
+            link: "https://wu-clan.github.io/homepage",
+          },
+          {
+            text: "Github 问题",
+            icon: "codicon:issues",
+            link: "https://github.com/fastapi-practices/fastapi-best-architecture/issues",
+          },
+          {
+            text: "Github 讨论",
+            icon: "mdi:discussion",
+            link: "https://github.com/fastapi-practices/fastapi-best-architecture/discussions",
+          },
+          {
+            text: "想法和建议",
+            icon: "mdi:discussion",
+            link: "https://discord.gg/xp8M6nY4NA",
+          },
+          {
+            text: "我要催更",
+            icon: "material-symbols:update-rounded",
+            link: "https://discord.gg/JyedBeHXkn",
+          },
+        ],
+      },
+      {
+        text: "课程",
+        items: [
+          {
+            text: "视频课程",
+            badge: { text: "已过时", type: "danger" },
+            icon: "ri:bilibili-fill",
+            link: "https://space.bilibili.com/284237214/lists",
+          },
+        ],
+      },
+      {
+        text: "动态",
+        items: [
+          {
+            text: "博客",
+            badge: { text: "充电站", type: "tip" },
+            icon: "fa7-solid:blog",
+            link: "/blog/",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    text: "插件系统",
+    icon: "unjs:unplugin",
+    items: [
+      {
+        text: "文档",
+        items: [
+          {
+            text: "前言",
+            icon: "bi:chat-text",
+            link: "/plugin/before",
+          },
+          {
+            text: "插件开发",
+            icon: "la:dev",
+            link: "/plugin/dev",
+          },
+          {
+            text: "插件分享",
+            icon: "material-symbols:share",
+            link: "/plugin/share",
+          },
+          {
+            text: "插件安装",
+            icon: "icon-park-outline:install",
+            link: "/plugin/install",
+          },
+        ],
+      },
+      {
+        text: "更多",
+        items: [
+          {
+            text: "插件市场",
+            icon: "mdi:marketplace-outline",
+            // badge: { text: 'HOT', type: 'danger' },
+            link: "/marketplace",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    text: "Pricing",
+    icon: "streamline-color:bag-dollar",
+    link: "/pricing",
+  },
+  {
+    text: "赞助",
+    icon: "material-icon-theme:github-sponsors",
+    link: "/sponsors",
+  },
+  {
+    text: "关于",
+    items: [
+      {
+        text: "常见问题",
+        icon: "healthicons:question",
+        link: "/questions.md",
+      },
+      {
+        text: "用户登记",
+        icon: "mdi:user-check",
+        link: "/users",
+      },
+      {
+        text: "社区团队",
+        icon: "fa-solid:user-friends",
+        link: "/team",
+      },
+      {
+        text: "隐私政策",
+        icon: "iconoir:privacy-policy",
+        link: "/privacy-policy",
+      },
+    ],
+  },
+]);
